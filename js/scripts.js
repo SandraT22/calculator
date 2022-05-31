b0d412dcfeb7f550b0d968f8adfdcf18a57b2b70
Sandra Tena <sandratena06@gmail.com>
2022-05-31 14:41:59 -0700

    Add subtract function to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,13 @@ function add(number1, number2) {
   
   const number1 = parseInt(prompt("enter a number:"));
   const number2 = parseInt(prompt("enter another number:"));
-  
-  alert(add(number1, number2));
+  const result = add(number1, number2);
+  alert(result);
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+  const subNum1 = parseInt(prompt("enter a number:"));
+  const subNum2 = parseInt(prompt("enter another number:"));
+  const subResult = subtract(subNum1, subNum2);
+  alert(subResult);
